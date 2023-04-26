41595ed1a952fcbb801d21ff35ae412783f0fa70
Lokesh Boddula <54484862+lokeshboddula@users.noreply.github.com>
2023-04-27 00:24:07 +0530

    understanding APIs level 1

---
+++ src/main/java/com/example/Student_Management_System/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.Student_Management_System;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+// in this we will write end points
+public class StudentController {
+
+    HashMap<Integer, Student> hashMap = new HashMap<>();
+    @PostMapping("/registerStudent")
+    public void addStudent(@RequestBody Student student) {
+        int key = student.getRollNo();
+        hashMap.put(key, student);
+    }
+
+    @GetMapping("/getStudentInfo")
+    public Student getStudent(@RequestParam("rollNo") Integer rollNo) {
+        Student student = hashMap.get(rollNo);
+        return student;
+    }
+}
